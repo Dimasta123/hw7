4a47602a5a9cacbb4315758b829d3bb69a94ade0
Dimasta123 <60709050+Dimasta123@users.noreply.github.com>
2020-12-05 19:02:29 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const ukraine = {tax: 0.195, middleSalary: 1789, vacancies: 11476};
+const latvia = {tax: 0.25, middleSalary: 1586, vacancies: 3921};
+const litva = {tax: 0.15, middleSalary: 1509, vacancies: 1114};
+
+function getTaxes(salary) {
+  return +(this.tax * salary).toFixed(2);
+}
+console.log(
+  `На Украине ты заплатишь столько налогов ${getTaxes.call(ukraine, 1900)}`,
+);
+function getMiddleTaxes(country) {
+  return +(this.tax * this.middleSalary).toFixed(2);
+}
+console.log(
+  `В Латвии айшники платят налог по средней зп ${getMiddleTaxes.call(latvia)}`,
+);
+function getTotalTaxes(country) {
+  return +(this.tax * this.middleSalary * this.vacancies).toFixed(2);
+}
+console.log(
+  `В литве всего со всей айти сферы набигает такая сумма налогов ${getTotalTaxes.call(litva)}`,
+);
+
+function getMySalary(country) {
+  const myMaxSalary = 2000;
+  const myMinSalary = 1500;
+  const salary = +(Math.random() * (myMaxSalary - myMinSalary + 1) +
+    myMinSalary).toFixed(2);
+  const myProfit = +(salary - this.tax * salary).toFixed(2);
+  return console.log({
+    mysalary: salary,
+    taxes: this.tax,
+    profit: myProfit,
+  });
+}
+
+setInterval(
+  function() {
+    getMySalary.call(ukraine);
+  },
+  10000,
+);
